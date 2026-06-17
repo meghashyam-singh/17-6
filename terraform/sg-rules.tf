41208b41884a80b2316da6da60c10f0ca2a433e0
megha <megha@gmail.com>
2026-06-17 22:31:51 +0530

    17-6

--- terraform/sg-rules.tf
+++ terraform/sg-rules.tf
@@ -7,3 +7,13 @@ resource "aws_security_group_rule" "catalogue_ingress_rule" {
     security_group_id = data.aws_ssm_parameter.catalogue_sg_id.value
     source_security_group_id = data.aws_ssm_parameter.frontend_alb_sg_id.value
 }
+
+resource "aws_security_group_rule" "user_ingress_rule" {
+    count = length(var.user_ingress_ports)
+    type = "ingress"
+    from_port = var.user_ingress_ports[count.index]
+    to_port = var.user_ingress_ports[count.index]
+    protocol = "tcp"
+    security_group_id = data.aws_ssm_parameter.user_sg_id.value
+    source_security_group_id = data.aws_ssm_parameter.frontend_alb_sg_id.value
+}
